4d439019a23376d5997f7ba66a9f7c6739ee1d08
Stanley Zheng <dev.stanzheng@gmail.com>
2016-11-28 08:55:36 -0500

    modify to map over the known range than try to guess by other means

--- go/pangram/pangram.go
+++ go/pangram/pangram.go
@@ -1,20 +1,23 @@
 package pangram
 
+import (
+	"unicode"
+)
+
 func IsPangram(sentence string) bool {
 	// Determines if a sentence is a panagram.
-	// A panagram is a sentance with only one of each letter
+	// A panagram is a sentence with only one of each letter
 	if len(sentence) == 0 {
 		return false
 	}
 	var letters = make(map[rune]bool)
 	for _, letter := range sentence {
-		if letter != 32 && letter < 'A' || letter > 'z' {
+		letters[unicode.ToLower(letter)] = true
+	}
+	for _,letter := range "abcdefghijklmnopqrstuvwxyz" {
+		if !letters[unicode.ToLower(letter)] {
 			return false
 		}
-		if letters[letter] == true {
-			return false
-		}
-		letters[letter] = true
 	}
 	return true
 }
