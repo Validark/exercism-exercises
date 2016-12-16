a71c65ed231c2b9940e11f44b80ce3ef21137eaa
Stanley Zheng <dev.stanzheng@gmail.com>
2016-12-16 10:30:11 -0500

    check some uppers in the characters

--- go/bob/bob.go
+++ go/bob/bob.go
@@ -1,3 +1,29 @@
 package bob // package name must match the package name in bob_test.go
+import "strings"
 
 const testVersion = 2 // same as targetTestVersion
+
+func Hey(what string) string {
+
+	what = strings.TrimSpace(what)
+	switch {
+	case len(what) == 0:
+		return "Fine. Be that way!"
+	case strings.ToUpper(what) == what && hasUpper(what):
+		return "Whoa, chill out!"
+	case what[len(what)-1] == '?':
+		return "Sure."
+	default:
+		return "Whatever."
+	}
+}
+
+// Checks if any characters are upper.
+func hasUpper(w string) bool {
+	for _, r := range w {
+		if r > 'A' && r < 'Z' {
+			return true
+		}
+	}
+	return false
+}
