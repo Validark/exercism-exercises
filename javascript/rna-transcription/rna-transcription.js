d4fef5fec8869eb09915c0cddb2f12d5ba7a481d
Stanley Zheng <dev.stanzheng@gmail.com>
2017-04-09 19:57:24 -0400

    cleaner

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -1,8 +1,14 @@
-const DNA_MAPPING = new Map([["C", "G"], ["G", "C"], ["A", "U"], ["T", "A"]]);
 class DnaTranscriber {
-  constructor(props) {}
+  constructor(props) {
+    this.DNA_MAPPING = new Map([
+      ["C", "G"],
+      ["G", "C"],
+      ["A", "U"],
+      ["T", "A"]
+    ]);
+  }
   toRna(seq) {
-    return seq.split("").map(s => DNA_MAPPING.get(s)).join("");
+    return seq.split("").map(s => this.DNA_MAPPING.get(s)).join("");
   }
 }
 
